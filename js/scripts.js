8b8e22d51976a2fda7eb25fe03644be35f572e6a
Vitaliy Sulimenko <prevetal@gmail.com>
2024-01-08 18:05:43 +0100

    +

--- js/scripts.js
+++ js/scripts.js
@@ -746,6 +746,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		$(this).toggleClass('active').closest('.reviews').find('.list .hide').slideToggle(300)
 	})
+
+
+	// Accordion
+	$('body').on('click', '.accordion .accordion_item .head', function(e) {
+		e.preventDefault()
+
+		let item = $(this).closest('.accordion_item'),
+			accordion = $(this).closest('.accordion')
+
+		if (item.hasClass('active')) {
+			item.removeClass('active').find('.data').slideUp(300)
+		} else {
+			accordion.find('.accordion_item').removeClass('active')
+			accordion.find('.data').slideUp(300)
+
+			item.addClass('active').find('.data').slideDown(300)
+		}
+	})
 })
 
 
