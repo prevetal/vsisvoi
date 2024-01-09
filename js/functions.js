e467ac9560b33503f732dfc8782cc77970caebc7
Vitaliy Sulimenko <prevetal@gmail.com>
2024-01-09 11:33:24 +0100

    +

--- js/functions.js
+++ js/functions.js
@@ -5,25 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 
 
-	// Lazy loading
-	const boxes = document.querySelectorAll('.lazyload')
-
-	function scrollTracking(entries) {
-		for (const entry of entries) {
-			if (entry.intersectionRatio >= 0.2 && entry.target.classList.contains('lazyload') && !entry.target.classList.contains('loaded')) {
-				entry.target.src = entry.target.getAttribute('data-src')
-				entry.target.classList.add('loaded')
-			}
-		}
-	}
-
-	const observer = new IntersectionObserver(scrollTracking, {
-		threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-	})
-
-	boxes.forEach(element => observer.observe(element))
-
-
 	// Setting the width of the standard scrollbar
 	document.documentElement.style.setProperty('--scroll_width', widthScroll() + 'px')
 
